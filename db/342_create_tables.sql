5936cd27748a178bc88255ddb6f7c9436a0c76f8
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-07-02 16:46:03 +0000

    Generated microservice version null

---
+++ db/342_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  commedit
+-- Automatically generated sql script for the service 342, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table hi.
+--
+CREATE TABLE commedit.hi (
+  hi INTEGER ,
+CONSTRAINT hi_PK PRIMARY KEY (hi)
+ 
+);
+
+
+
